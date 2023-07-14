5ed3cce5426f615593341940dd383a03f54043db
Shivansh Raghav <shivanshraghav7@gmai.com>
2023-07-15 00:12:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-// complete this js code
 function Person(name, age) {
 	this.name=name
 	this.age=age
 }
-Person.prototype.greet={
+Person.prototype.greet=function(){
 	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
 }
 
 function Employee(name, age, jobTitle) {
-	Person(name,age).call(this)
+	Person.call(this,name,age)
 	this.jobTitle=jobTitle
 }
 Employee.prototype.__proto__=Person.prototype
-Employee.prototype.jobGreet={
+Employee.prototype.jobGreet=function(){
 	console.log(`Hello, my name is ${this.age}, I am ${this.jobTitle} years old.`)
 }
 // Do not change code below this line
